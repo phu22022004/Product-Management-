e525f6c8a5041e7d6afffbdb11d08877f40157d5
phu22022004 <phuhmpx9zz@gmail.com>
2026-06-03 21:24:52 +0700

    tối ưu phần filter-status

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,32 +1,8 @@
 const Product = require("../../models/product.model");
+const filterStatusHelper = require("../../helper/filter-status.js");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  let filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: "",
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: "",
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: "",
-    },
-  ];
-  if (req.query.status) {
-    const index = filterStatus.findIndex(
-      (item) => item.status == req.query.status,
-    );
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex((item) => item.status == "");
-    filterStatus[index].class = "active";
-  }
+  const filterStatus = filterStatusHelper(req.query);
 
   let find = {
     deleted: false,

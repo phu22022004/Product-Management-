b8ac07b2c7ac68ccf2169de18badd9c7ce616697
phu22022004 <phuhmpx9zz@gmail.com>
2026-06-13 16:57:31 +0700

    Thay đổi trạng thái 1 sản phẩm

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productController = require("../../controllers/admin/product.controller");
 
 router.get("/", productController.index);
+router.patch("/change-status/:status/:id", productController.changeStatus);
 
 module.exports = router;

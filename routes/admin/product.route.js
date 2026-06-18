b2e404173cbf4ac7078854f2dd4c5a30dd8b7fa4
phu22022004 <phuhmpx9zz@gmail.com>
2026-06-18 22:24:06 +0700

    Tối ưu phần chọn ảnh

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const multer = require("multer");
+const storageMulti = require("../../helper/storageMulter");
+const upload = multer({ storage: storageMulti() });
 const router = express.Router();
 const productController = require("../../controllers/admin/product.controller");
 
@@ -7,5 +10,9 @@ router.patch("/change-status/:status/:id", productController.changeStatus);
 router.patch("/change-multi", productController.changeMulti);
 router.delete("/delete/:id", productController.deleteItem);
 router.get("/create", productController.create);
-router.post("/create", productController.createPost);
+router.post(
+  "/create",
+  upload.single("thumbnail"),
+  productController.createPost,
+);
 module.exports = router;

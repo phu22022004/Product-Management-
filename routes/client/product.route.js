d611e0825e3915d736e6bd91ec946b14e67e4646
phu22022004 <phuhmpx9zz@gmail.com>
2026-07-01 16:13:36 +0700

    Trang chi tiết sản phẩm bên client

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const productController = require("../../controllers/client/product.controller");
 
 router.get("/", productController.index);
-
+router.get("/:slug", productController.detail);
 module.exports = router;

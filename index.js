b5fa3673b22f0ef2476cb98a08579711fde10244
phu22022004 <phuhmpx9zz@gmail.com>
2026-07-03 20:31:14 +0700

    Deploy project lên vercel

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use(methodOverride("_method"));
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
 //Flash
 app.use(cookieParser("phuIT"));

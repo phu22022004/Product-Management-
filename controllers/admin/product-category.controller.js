bd221fae9c405d95e9640cb67abfe72ace4e8652
phu22022004 <phuhmpx9zz@gmail.com>
2026-07-23 21:05:14 +0700

    Xây dựng phần quản lí danh mục sản phẩm (p2).

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,22 +1,30 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system.js");
+const createTreeHelper = require("../../helper/create-tree.js");
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
   const records = await ProductCategory.find(find);
-
+  const newRecords = createTreeHelper.tree(records);
   res.render("admin/pages/products-category/index.pug", {
     pageTitle: "Danh mục sản phẩm",
-    records: records,
+    records: newRecords,
   });
 };
 
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  const records = await ProductCategory.find(find);
+  const newRecords = createTreeHelper.tree(records);
   res.render("admin/pages/products-category/create.pug", {
     pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords,
   });
 };
 

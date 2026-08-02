e52ee61e6f40e6892796418739ab9e4fd14877f8
phu22022004 <phuhmpx9zz@gmail.com>
2026-08-02 20:52:33 +0700

    Xây dựng phần chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -26,3 +26,39 @@ module.exports.createPost = async (req, res) => {
   await record.save();
   res.redirect(`${systemConfig.prefixAdmin}/roles`);
 };
+
+// [GET]/admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let find = {
+      _id: id,
+      deleted: false,
+    };
+    const data = await Role.findOne(find);
+
+    res.render("admin/pages/roles/edit", {
+      pageTitlte: "Sửa nhóm quyền",
+      data: data,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  }
+};
+
+// [PATCH]/admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Role.updateOne({ _id: id }, req.body);
+    req.flash("success", "Cập nhật nhóm quyền thành công!");
+  } catch (error) {
+    req.flash("error", "Cập nhật nhóm quyền thất bại!");
+  }
+  const referer = req.get("Referrer") || req.get("Referer");
+  const redirectUrl =
+    referer || req.baseUrl || `${systemConfig.prefixAdmin}/roles`;
+
+  res.redirect(redirectUrl);
+};
--- routes/admin/roles.route.js
+++ routes/admin/roles.route.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const roleController = require("../../controllers/admin/role.controller.js");
+
 router.get("/", roleController.index);
 router.get("/create", roleController.create);
 router.post("/create", roleController.createPost);
+router.get("/edit/:id", roleController.edit);
+router.patch("/edit/:id", roleController.editPatch);
+
 module.exports = router;

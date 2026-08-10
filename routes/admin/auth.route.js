6980f1e0ddccfea7770c1714196a45afd6e093ea
phu22022004 <phuhmpx9zz@gmail.com>
2026-08-10 21:07:57 +0700

    Tính năng đăng xuất

--- routes/admin/auth.route.js
+++ routes/admin/auth.route.js
@@ -4,4 +4,5 @@ const authController = require("../../controllers/admin/auth.controller");
 const authValidate = require("../../validates/admin/auth.validate");
 router.get("/login", authController.login);
 router.post("/login", authValidate.loginPost, authController.loginPost);
+router.get("/logout", authController.logout);
 module.exports = router;

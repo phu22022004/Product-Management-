5775339a363f1714a61713364b58b200325cca49
unknown <phuhmpx9zz@gmail.com>
2026-09-16 22:23:34 +0700

    Trang kết quả tìm kiếm bên client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,10 @@
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const ProductRoutes = require("./product.route");
 const HomeRoutes = require("./home.route");
+const searchRoutes = require("./search.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category)
   app.use("/", HomeRoutes);
   app.use("/products",ProductRoutes);
+  app.use("/search",searchRoutes);
 };

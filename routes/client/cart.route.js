7ff785fe8be3f8cd021028ae67888cee5f30a81e
unknown <phuhmpx9zz@gmail.com>
2026-09-17 17:10:38 +0700

    Làm trang giỏ hàng

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const cartController = require("../../controllers/client/cart.controller");
+router.get("/",cartController.index);
 router.post("/add/:productId", cartController.addPost);
 module.exports = router;

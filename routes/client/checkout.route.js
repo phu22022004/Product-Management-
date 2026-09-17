34d4bcc6efbde557258a28724290c08e671d9760
unknown <phuhmpx9zz@gmail.com>
2026-09-17 23:14:26 +0700

    lấy data được gửi lên lưu vào database

--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const checkoutController = require("../../controllers/client/checkout.controller");
 router.get("/", checkoutController.index);
+router.post("/order", checkoutController.order);
 module.exports = router;

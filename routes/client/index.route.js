5cb6485f79200d36f6bd439d6b95008f0c5dbe65
unknown <phuhmpx9zz@gmail.com>
2026-09-17 23:00:04 +0700

    Làm trang giao diện bên order

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -5,6 +5,7 @@ const ProductRoutes = require("./product.route");
 const HomeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category)
   app.use(cartMiddleware.cartId)
@@ -12,4 +13,5 @@ module.exports = (app) => {
   app.use("/products",ProductRoutes);
   app.use("/search",searchRoutes);
   app.use("/cart",cartRoutes);
+  app.use("/checkout",checkoutRoutes);
 };

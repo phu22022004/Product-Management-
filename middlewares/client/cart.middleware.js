ad8cf03373bc685e42cd5d5e0a71540e08312b33
unknown <phuhmpx9zz@gmail.com>
2026-09-17 16:22:04 +0700

    Làm nút mini Cart trên header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -12,6 +12,11 @@ module.exports.cartId = async (req,res,next)=>{
         });
     }else{
         //khi đã có giỏ hàng
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId,
+        })
+        cart.totalQuantity = cart.products.reduce((sum,item)=>sum+item.quantity,0)
+        res.locals.miniCart = cart;
     }
     next();
 }
